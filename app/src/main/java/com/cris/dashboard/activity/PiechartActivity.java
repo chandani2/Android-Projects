95d47cd845c2087b0ebd9dc8d8ef89c472fd23d2
chandani2 <chandani3103@gmail.com>
2022-07-04 15:58:46 +0530

    change TextView Id Name

--- app/src/main/java/com/cris/dashboard/activity/PiechartActivity.java
+++ app/src/main/java/com/cris/dashboard/activity/PiechartActivity.java
@@ -9,18 +9,15 @@ import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.TextView;
-
 import com.cris.dashboard.R;
-
 import org.eazegraph.lib.charts.PieChart;
 import org.eazegraph.lib.models.PieModel;
-
 import org.eazegraph.lib.models.PieModel;
 
 import java.util.ArrayList;
 
 public class PiechartActivity extends AppCompatActivity {
-    TextView tvR, tvPython, tvCPP;
+    TextView tvjourney, tvseason, tvplatform;
     PieChart pieChart;
     String journey, season, platform;
 
@@ -32,9 +29,9 @@ public class PiechartActivity extends AppCompatActivity {
         // Link those objects with their
         // respective id's that
         // we have given in .XML file
-        tvR = findViewById(R.id.tvR);
-        tvPython = findViewById(R.id.tvPython);
-        tvCPP = findViewById(R.id.tvCPP);
+        tvjourney = findViewById(R.id.tvjourney);
+        tvseason = findViewById(R.id.tvseason);
+        tvplatform = findViewById(R.id.tvplatform);
         pieChart = findViewById(R.id.piechart);
 
         setTitle("Pie Chart Activity");
@@ -59,25 +56,25 @@ public class PiechartActivity extends AppCompatActivity {
 
 
         // Set the percentage of language used
-        tvR.setText(journey);
-        tvPython.setText(season);
-        tvCPP.setText(platform);
+        tvjourney.setText(journey);
+        tvseason.setText(season);
+        tvplatform.setText(platform);
 
         // Set the data and color to the pie chart
         pieChart.addPieSlice(
                 new PieModel(
                         "Journey",
-                        Integer.parseInt(tvR.getText().toString()),
+                        Integer.parseInt(tvjourney.getText().toString()),
                         Color.parseColor("#FFA726")));
         pieChart.addPieSlice(
                 new PieModel(
                         "Season",
-                        Integer.parseInt(tvPython.getText().toString()),
+                        Integer.parseInt(tvseason.getText().toString()),
                         Color.parseColor("#66BB6A")));
         pieChart.addPieSlice(
                 new PieModel(
                         "Platform",
-                        Integer.parseInt(tvCPP.getText().toString()),
+                        Integer.parseInt(tvplatform.getText().toString()),
                         Color.parseColor("#EF5350")));
 
         // To animate the pie chart
